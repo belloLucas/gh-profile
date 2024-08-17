f7f102a4b6ca91623183a6ba73bbfe1ff8a491b9
belloLucas <lucasbellodev@gmail.com>
2024-08-17 18:13:48 -0300

    project started

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import CardComponent from './components/CardComponent.vue'
+</script>
+
+<template>
+  <div class="wrapper">
+    <CardComponent />
+  </div>
+</template>
+
+<style scoped>
+  .wrapper {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+</style>
---
+++ src/components/CardComponent.vue
@@ -0,0 +1,38 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="card">
+        <h1>Busque por um perfil</h1>
+        <input type="text" placeholder="Digite o nome de usuário" />
+    </div>
+    <div class="card">
+        <h1 class="login title">Lucas Bello</h1>
+        <h4 class="username">belloLucas</h4>
+        <h4 class="location">Juiz de Fora</h4>
+        <h4 class="bio">Desenvolvedor Front-end</h4>
+        <div class="followers">
+            <h5 class="followers">15</h5>
+            <h5 class="following">24</h5>
+        </div>
+    </div>
+</template>
+
+<style scoped lang="scss">
+    .card {
+        padding: 20px;
+        border-radius: 5px;
+        text-align: center;
+        background-color: #f4f4f4;
+        box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+
+        input {
+            width: 100%;
+            padding: 10px;
+            margin-top: 10px;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+        }
+    }
+</style>
