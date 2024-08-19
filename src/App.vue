d9a17a57ab12cec8bda82dd5920a7d3b7d6bf7a0
belloLucas <lucasbellodev@gmail.com>
2024-08-18 21:01:47 -0300

    refactor: changing styles

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,10 @@ import CardComponent from './components/CardComponent.vue'
 
 <style scoped>
   .wrapper {
+    display: flex;
+    justify-content: center;
+    width: 100%;
+    
     position: absolute;
     top: 50%;
     left: 50%;
